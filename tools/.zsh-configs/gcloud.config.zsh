f7b0add33bd8a0040ea4b447800c1f351991df59
Malcolm Burtenshaw <malcolm@gosolo.io>
2024-01-29 14:54:26 -0700

    chore: add google ADC path

--- tools/.zsh-configs/gcloud.config.zsh
+++ tools/.zsh-configs/gcloud.config.zsh
@@ -1,4 +1,6 @@
 # [Google Cloud SDK](https://cloud.google.com/sdk/docs)
 
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/google_application_credentials.json"
+
 if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then source "${HOME}/google-cloud-sdk/path.zsh.inc"; fi
 if [ -f "${HOME}/google-cloud-sdk/completion.zsh.inc" ]; then source "${HOME}/google-cloud-sdk/completion.zsh.inc"; fi
