da9bdff1b7ff3e33d9e541672deaee5af0004be4
Malcolm Burtenshaw <malcolm@gosolo.io>
2024-02-06 16:19:05 -0700

    build: export new variable for solo

--- tools/.zsh-configs/gosolo.config.zsh
+++ tools/.zsh-configs/gosolo.config.zsh
@@ -4,3 +4,5 @@ export solohome="${HOME}/code/gitlab.com/gosolo"
 
 export SOLO_HOME="${HOME}/code/gitlab.com/gosolo/solo"
 [ -s "${SOLO_HOME}/scripts/local/.bashrc_aliases" ] && source "${SOLO_HOME}/scripts/local/.bashrc_aliases"
+
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
