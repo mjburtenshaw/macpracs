2f02ab316b52fc3d74e045c81243a250094d2f74
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2024-04-12 22:52:08 -0600

    chore: Move halo config home location

--- tools/.zsh-configs/halo-devs.config.zsh
+++ tools/.zsh-configs/halo-devs.config.zsh
@@ -1,7 +1,6 @@
 # [`halo-devs`](https://gitlab.com/halo-devs)
 
-export halo_home="${HOME}/code/gitlab.com/halo-devs"
-source $halo_home/dx/scripts/aliases.sh
+source ~/.config/halo/config.sh
 
 alias ssh-halo-api="ssh -i ${HOME}/.secrets/halo/halo-api.pem ubuntu@ec2-52-53-212-132.us-west-1.compute.amazonaws.com"
 alias ssh-halo-marketing-site="ssh -i ${HOME}/.secrets/halo/halo-marketing-site.pem ubuntu@ec2-13-57-232-14.us-west-1.compute.amazonaws.com"
